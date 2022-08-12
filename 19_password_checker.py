3bcec336568f4cc735b0bf77aee43aa36473a009
aditya634 <70478257+aditya634@users.noreply.github.com>
2022-08-12 22:01:31 +0530

    update

---
+++ 19_password_checker.py
@@ -0,0 +1,10 @@
+# Password Checker
+
+user=input(" Enter Username :")
+
+pas = input("Enter password :")
+
+print(f"Your username is : {user}") 
+hidden_pass= len(pas)
+print("your password is : {hidden_pass}")
+print('*' * hidden_pass) 
